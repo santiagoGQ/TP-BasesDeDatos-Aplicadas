34759b2b16c3572df36583e69ed095fdfe517858
santiagoGQ <santi_gq@yahoo.com.ar>
2025-10-25 09:45:44 -0300

    Cambios de nombres de archivos, agregado script de creacion de funciones

--- scripts/01_CreacionFunciones.sql
+++ scripts/01_CreacionFunciones.sql
@@ -1,11 +1,26 @@
+--------------------------------------------------
+-- BASES DE DATOS APLICADAS
+--  GRUPO 04
+--  INTEGRANTES
+--   CONDE, FRANCO
+--   GARAY QUINTERO, SANTIAGO
+--   SIMCIC, TOBIAS
+--------------------------------------------------
+
+-------------CREACION DE LAS FUNCIONES------------
+
+--Cambia a COM2900_G04
+USE COM2900_G04
+GO
+
 CREATE FUNCTION adm.fn_QuitarEspaciosEmail(
-    @email VARCHAR(100)
+    @email NVARCHAR(50)
 )
-RETURNS VARCHAR(100)
+RETURNS NVARCHAR(50)
 AS
 BEGIN
     -- Elimina espacios al principio y al final
-    DECLARE @resultado VARCHAR(100) = LTRIM(RTRIM(@email));
+    DECLARE @resultado NVARCHAR(50) = LTRIM(RTRIM(@email));
 
     -- Elimina espacios intermedios
     SET @resultado = REPLACE(@resultado, ' ', '');
