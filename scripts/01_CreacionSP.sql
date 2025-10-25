4a6a289c002babd73fe47087e614dc24c93479bb
santiagoGQ <santi_gq@yahoo.com.ar>
2025-10-24 23:52:50 -0300

    Agregado SP para proveedor de limpieza y servicio publico

---
+++ scripts/01_CreacionSP.sql
@@ -0,0 +1,16 @@
+CREATE OR ALTER PROCEDURE adm.AgregarTipoServicioLimpieza 
+	@nombre VARCHAR(45)
+AS
+BEGIN
+	INSERT INTO adm.TipoServicioLimpieza(nombre) values (@nombre)
+END
+GO
+
+CREATE OR ALTER PROCEDURE adm.AgregarTipoServicioPublico 
+	@nombre VARCHAR(45)
+AS
+BEGIN
+	INSERT INTO adm.TipoServicioPublico(nombre) values (@nombre)
+END
+GO
+
