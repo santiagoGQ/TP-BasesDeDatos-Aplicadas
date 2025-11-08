9321a290022fb2fcbc1379a8497bb63986f30603
Franco <franconde@hotmail.com.ar>
2025-11-08 17:02:05 -0300

    update reportes

--- scripts/05_CreacionIndexReportes.sql
+++ scripts/05_CreacionIndexReportes.sql
@@ -13,36 +13,52 @@ USE COM2900_G04
 GO
 
 -------------CREACION DE LOS INDICES-------------
---REPORTE 1 y 2 (flujo semanal y total UF por mes)  
+-- REPORTE 1 (flujo semanal)
+IF NOT EXISTS (SELECT 1 FROM sys.indexes WHERE name = 'IX_Pago_Fecha_CBU')
+BEGIN
+    CREATE NONCLUSTERED INDEX IX_Pago_Fecha_CBU
+    ON fin.Pago (fecha, cbu_cvu)
+    INCLUDE (monto)
+END
+GO
+
+IF NOT EXISTS (SELECT 1 FROM sys.indexes WHERE name = 'IX_UF_Consorcio_CBU')
+BEGIN
+    CREATE NONCLUSTERED INDEX IX_UF_Consorcio_CBU
+    ON adm.UnidadFuncional (id_consorcio, cbu)
+END
+GO
+
+-- REPORTE 2 (recaudación mensual por UF)
 IF NOT EXISTS (SELECT 1 FROM sys.indexes WHERE name = 'IX_Expensa_Consorcio_Fecha')
 BEGIN
     CREATE NONCLUSTERED INDEX IX_Expensa_Consorcio_Fecha
-    ON adm.Expensa (id_consorcio, fechaGenerado);
-END;
+    ON adm.Expensa (id_consorcio, fechaGenerado)
+END
 GO
 
-IF NOT EXISTS (SELECT 1 FROM sys.indexes WHERE name = 'IX_EstadoCuenta_UF_Pago')
+IF NOT EXISTS (SELECT 1 FROM sys.indexes WHERE name = 'IX_EstadoCuenta_UF_Expensa')
 BEGIN
-    CREATE NONCLUSTERED INDEX IX_EstadoCuenta_UF_Pago
-    ON fin.EstadoDeCuenta (id_uni_func, pago_recibido)
-    INCLUDE (expensas_ordinarias, expensas_extraordinarias, deuda);
-END;
+    CREATE NONCLUSTERED INDEX IX_EstadoCuenta_UF_Expensa
+    ON fin.EstadoDeCuenta (id_uni_func, id_expensa)
+    INCLUDE (pago_recibido, expensas_ordinarias, expensas_extraordinarias, deuda)
+END
 GO
 
---REPORTE 3 (recaudación por procedencia)
-IF NOT EXISTS (SELECT 1 FROM sys.indexes WHERE name = 'IX_EstadoCuenta_Expensa_Anio')
+-- REPORTE 3 (recaudación por procedencia)
+IF NOT EXISTS (SELECT 1 FROM sys.indexes WHERE name = 'IX_Expensa_Consorcio_AnioMes')
 BEGIN
-    CREATE NONCLUSTERED INDEX IX_EstadoCuenta_Expensa_Anio
-    ON fin.EstadoDeCuenta (id_expensa)
-    INCLUDE (expensas_ordinarias, expensas_extraordinarias, total_a_pagar);
-END;
+    CREATE NONCLUSTERED INDEX IX_Expensa_Consorcio_AnioMes
+    ON adm.Expensa (id_consorcio, fechaGenerado)
+    INCLUDE (id_expensa)
+END
 GO
 
--- REPORTE 6 (días entre pagos por UF)
+-- REPORTE 6 (días entre pagos)
 IF NOT EXISTS (SELECT 1 FROM sys.indexes WHERE name = 'IX_Pago_UF_Fecha')
 BEGIN
     CREATE NONCLUSTERED INDEX IX_Pago_UF_Fecha
     ON fin.Pago (id_uni_func, fecha)
-    INCLUDE (monto, asociado);
-END;
+    INCLUDE (monto, asociado)
+END
 GO
--- scripts/05_CreacionSPreportes.sql
+++ scripts/05_CreacionSPreportes.sql
@@ -16,16 +16,16 @@ GO
 
 ---------REPORTE 1 - Flujo de caja semanal---------
 CREATE OR ALTER PROCEDURE rep.Uno_FlujoSemanal
-	@id_consorcio INT, @anio INT
+	@id_consorcio INT, @anio INT, @mes_desde INT, @mes_hasta INT
 AS
 BEGIN
 	SET NOCOUNT ON;
 
 	BEGIN TRY
 		--Validacion de parametros de entrada
-		IF @id_consorcio IS NULL OR @anio IS NULL
+		IF @id_consorcio IS NULL OR @anio IS NULL OR @mes_desde IS NULL OR @mes_hasta IS NULL
 		BEGIN
-			RAISERROR('Debe especificar un consorcio y un año.',16,1)
+			RAISERROR('Debe especificar valores para cada parametro.',16,1)
 			RETURN
 		END
 
@@ -35,52 +35,55 @@ BEGIN
 			RETURN
 		END
 
+		IF @mes_desde<1 OR @mes_desde>12 OR @mes_hasta<1 OR @mes_hasta>12
+		BEGIN
+			RAISERROR('Los meses deben ser valores entre 1 y 12.',16,1)
+			RETURN
+		END
+
+		IF @mes_desde>@mes_hasta
+		BEGIN
+			RAISERROR('Solapamiento en los meses ingresados.',16,1)
+			RETURN
+		END
+
 		IF @anio IS NULL OR @anio> YEAR(GETDATE())
 		BEGIN
 			RAISERROR('Debe especificar una fecha valida.',16,1)
 			RETURN
 		END;
 
-		--CTE 1 - Ingresos semanales (pagos recibidos)
-		WITH CTE_Ingresos AS(
-			SELECT
-				YEAR(e.fechaGenerado) AS Anio,
-				DATEPART(WEEK,e.fechaGenerado) AS Semana,
-				SUM(ISNULL(ec.pago_recibido,0)) AS TotalIngresos
-			FROM fin.EstadoDeCuenta ec
-			JOIN adm.Expensa e
-				ON e.id_expensa = ec.id_expensa
-			JOIN fin.vista_UFyConsorcio uf
-				ON uf.id_uni_func = ec.id_uni_func
-			WHERE uf.id_consorcio = @id_consorcio
-				  AND YEAR(e.fechaGenerado) = @anio
-			GROUP BY YEAR(e.fechaGenerado), DATEPART(WEEK,e.fechaGenerado)),
-
-		--CTE 2 - Egresos semanales (gastos)
-		CTE_Egresos AS(
-			SELECT
-				YEAR(f.fecha_de_cierre) AS Anio,
-				DATEPART(WEEK,f.fecha_de_cierre) AS Semana,
-				SUM(ISNULL(f.total_gastado,0)) AS TotalEgresos
-				FROM fin.vista_GastosPorExpensa f
-				WHERE f.id_consorcio = @id_consorcio
-					AND YEAR(f.fecha_de_cierre) = @anio
-				GROUP BY YEAR(f.fecha_de_cierre),DATEPART(WEEK,f.fecha_de_cierre))
-
-		--
-		SELECT
-			ISNULL(i.Anio,e.Anio) AS Anio,
-			ISNULL(i.Semana, e.Semana) AS Semana,
-			ISNULL(i.TotalIngresos,0) AS TotalIngresos,
-			ISNULL(e.TotalEgresos,0) AS TotalEgresos,
-			AVG(ISNULL(i.TotalIngresos,0) - ISNULL(e.TotalEgresos,0))
-				OVER() AS PromedioSemanal,
-			SUM(ISNULL(i.TotalIngresos,0) - ISNULL(e.TotalEgresos,0))
-				OVER(ORDER BY ISNULL(i.Semana,e.Semana)) AS Acumulado
-		FROM CTE_Ingresos i
-		FULL JOIN CTE_Egresos e
-		ON i.Anio = e.Anio AND i.Semana = e.Semana
-		ORDER BY Anio,Semana
+		--CTE 1 - Recaudacion semanal (pagos recibidos)
+        ;WITH CTE_Pagos AS (
+            SELECT
+				uf.id_consorcio AS Consorcio,
+                uf.id_uni_func AS Unidad_Funcional,
+                uf.piso AS Piso,
+                uf.depto AS Depto,
+                uf.cbu AS CBU_Unidad,
+                DATEPART(WEEK, p.fecha) AS Semana,
+                DATENAME(MONTH, p.fecha) AS Mes,
+                SUM(p.monto) AS TotalPagado
+            FROM fin.Pago p
+            INNER JOIN adm.UnidadFuncional uf
+                ON uf.cbu = p.cbu_cvu
+            WHERE uf.id_consorcio = @id_consorcio
+              AND YEAR(p.fecha) = @anio
+              AND MONTH(p.fecha) BETWEEN @mes_desde AND @mes_hasta
+            GROUP BY uf.id_consorcio,uf.id_uni_func, uf.piso, uf.depto, uf.cbu,
+                     DATEPART(WEEK, p.fecha), DATENAME(MONTH, p.fecha)
+        ),
+        CTE_RecaudacionSemanal AS (
+            SELECT
+				Consorcio,
+                Semana,
+                SUM(TotalPagado) AS TotalSemanal,
+                CAST(AVG(SUM(TotalPagado)) OVER() AS DECIMAL(18,2)) AS PromedioSemanal,
+                SUM(SUM(TotalPagado)) OVER(ORDER BY Semana) AS Acumulado
+            FROM CTE_Pagos
+            GROUP BY Consorcio,Semana
+        )
+	SELECT * FROM CTE_RecaudacionSemanal r ORDER BY r.Semana
 	END TRY
 	BEGIN CATCH
 		PRINT('Error al generar el reporte: ' + ERROR_MESSAGE())
@@ -90,7 +93,7 @@ GO
 
 ---------REPORTE 2 - Recaudacion por UF por mes---------
 CREATE OR ALTER PROCEDURE rep.Dos_TotalUFporMes
-	@id_consorcio INT, @anio INT
+	@id_consorcio INT, @anio INT, @id_uf INT=NULL
 AS
 BEGIN
 	SET NOCOUNT ON;
@@ -113,6 +116,13 @@ BEGIN
 		BEGIN
 			RAISERROR('Debe especificar una fecha valida.',16,1)
 			RETURN
+		END
+
+		IF @id_uf IS NOT NULL
+		AND NOT EXISTS(SELECT 1 FROM fin.Vista_UfYConsorcio WHERE id_uni_func=@id_uf)
+		BEGIN
+			RAISERROR('No existe la unidad funcional ingresada.',16,1)
+			RETURN
 		END;
 
 		--CTE Recaudacion
@@ -129,6 +139,7 @@ BEGIN
 				ON uf.id_uni_func = ec.id_uni_func
 			WHERE uf.id_consorcio = @id_consorcio
 				  AND YEAR(e.fechaGenerado) = @anio
+				  AND (@id_uf IS NULL OR uf.id_uni_func=@id_uf)
 			GROUP BY uf.id_uni_func, DATENAME(MONTH, e.fechaGenerado))
 
 		--Pivot del CTE Recaudacion
@@ -146,7 +157,7 @@ GO
 
 ---------REPORTE 3 - Recaudacion por origen---------
 CREATE OR ALTER PROCEDURE rep.Tres_RecaudacionPorProcedencia
-	@id_consorcio INT, @anio INT
+	@id_consorcio INT, @anio INT, @mes_desde INT, @mes_hasta INT
 AS
 BEGIN
 	SET NOCOUNT ON;
@@ -181,6 +192,7 @@ BEGIN
 		JOIN adm.Expensa ex ON ex.id_expensa = ec.id_expensa
 		WHERE (ex.id_consorcio = @id_consorcio)
 		  AND YEAR(ex.fechaGenerado)=@anio
+		  AND MONTH(ex.fechaGenerado) BETWEEN @mes_desde AND @mes_hasta
 		GROUP BY ex.fechaGenerado
 		ORDER BY Periodo
 	END TRY
@@ -191,9 +203,9 @@ BEGIN
 END
 GO
 
----------REPORTE 4 - TOP 5 meses con mayores gastos e ingresos---------
+---------REPORTE 4 - TOP 5 (o el deseado) meses con mayores gastos e ingresos---------
 CREATE OR ALTER PROCEDURE rep.Cuatro_TopMeses
-	@id_consorcio INT, @anio INT
+	@id_consorcio INT, @anio INT, @top INT
 AS
 BEGIN
 	SET NOCOUNT ON;
@@ -208,6 +220,17 @@ BEGIN
 		IF NOT EXISTS (SELECT 1 FROM adm.Consorcio WHERE id_consorcio=@id_consorcio)
 		BEGIN
 			RAISERROR('No existe consorcio con ese id.',16,1)
+			RETURN
+		END
+
+		IF @top IS NULL
+		BEGIN
+			SET @top=5
+		END
+
+		IF @top <1
+		BEGIN
+			RAISERROR('Debe especificar un valor valido para el top.',16,1)
 			RETURN
 		END
 
@@ -248,7 +271,7 @@ BEGIN
 		   GROUP BY YEAR(e.fechaGenerado), MONTH(e.fechaGenerado), DATENAME(MONTH, e.fechaGenerado))
 
 		--
-		SELECT TOP 5 
+		SELECT TOP (@top)
 			ISNULL(i.Anio, e.Anio) AS Anio,
 			ISNULL(i.NombreMes, e.NombreMes) AS Mes,
 			ISNULL(i.TotalIngresos, 0) AS TotalIngresos,
@@ -271,7 +294,7 @@ GO
 
 ---------REPORTE 5 - TOP 3 propietarios con morosidad---------
 CREATE OR ALTER PROCEDURE rep.Cinco_TopMora
-	@id_consorcio INT
+	@id_consorcio INT, @piso VARCHAR(2)=NULL, @top INT
 AS
 BEGIN
 	SET NOCOUNT ON;
@@ -287,13 +310,31 @@ BEGIN
 		BEGIN
 			RAISERROR('No existe consorcio con ese id.',16,1)
 			RETURN
+		END
+
+		IF @top IS NULL
+		BEGIN
+			SET @top=5
+		END
+
+		IF @top <1
+		BEGIN
+			RAISERROR('Debe especificar un valor valido para el top.',16,1)
+			RETURN
+		END
+
+		IF @piso IS NOT NULL
+        AND NOT EXISTS (SELECT 1 FROM fin.Vista_UfYConsorcio WHERE piso = @piso AND id_consorcio = @id_consorcio)
+		BEGIN
+			RAISERROR('Debe especificar un piso existente.',16,1)
+			RETURN
 		END;
 
 		--CTE Deudas de propietario
 		WITH CTE_Deuda AS(
 			SELECT
 				--datos propietario
-				p.nombre, p.apellido,p.dni, p.email, p.telefono,
+				p.nombre, p.apellido,p.dni, p.email, p.telefono, uf.piso,
 				SUM(ISNULL(ec.deuda,0)) AS deuda
 				FROM fin.EstadoDeCuenta ec
 				JOIN fin.Vista_UfYConsorcio uf
@@ -301,12 +342,13 @@ BEGIN
 				JOIN adm.Propietario p
 					ON p.id_prop=uf.id_prop
 				WHERE uf.id_consorcio=@id_consorcio
-				GROUP BY p.dni, p.nombre, p.apellido, p.email, p.telefono)
+					AND (@piso IS NULL  OR uf.piso=@piso)
+				GROUP BY p.dni, p.nombre, p.apellido, p.email, p.telefono, uf.piso)
 
 		--
-		SELECT TOP 3
+		SELECT TOP (@top)
 			c.apellido+', '+c.nombre AS Propietario,
-			c.dni,c.email, c.telefono, c.deuda
+			c.dni,c.email, c.telefono, c.deuda, c.piso
 		FROM CTE_Deuda c
 		ORDER BY c.deuda ASC
 		FOR XML PATH('Propietario'), ROOT('TopMorosos'),ELEMENTS;
@@ -318,7 +360,7 @@ END
 GO
 
 CREATE OR ALTER PROCEDURE rep.Seis_DiasPagosUF
-	@id_consorcio INT, @anio INT
+	@id_consorcio INT, @anio INT, @piso NVARCHAR(2)=NULL
 AS
 BEGIN
 	SET NOCOUNT ON;
@@ -342,6 +384,13 @@ BEGIN
 			RETURN
 		END;
 
+		IF @piso IS NOT NULL
+        AND NOT EXISTS (SELECT 1 FROM fin.Vista_UfYConsorcio WHERE piso = @piso AND id_consorcio = @id_consorcio)
+		BEGIN
+			RAISERROR('Debe especificar un piso existente.',16,1)
+			RETURN
+		END;
+
 		WITH PagosOrd AS(
 			SELECT
 				uf.id_uni_func AS Unidad_Funcional,
@@ -352,7 +401,10 @@ BEGIN
 			FROM fin.Pago p
 			JOIN adm.UnidadFuncional uf ON uf.id_uni_func = p.id_uni_func
 			WHERE
-				uf.id_consorcio=@id_consorcio AND YEAR(p.fecha) = @anio AND p.asociado = 1)
+				uf.id_consorcio=@id_consorcio 
+				AND YEAR(p.fecha) = @anio 
+				AND p.asociado = 1
+				AND (@piso IS NULL OR uf.piso=@piso))
 		SELECT * FROM PagosOrd
 		ORDER BY Unidad_Funcional, Fecha_de_Pago;
 	END TRY
